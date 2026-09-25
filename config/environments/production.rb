96237f1d83362bd720fde56401e2333c12a99185
hiro <m.hirohata.gero1219@gmail.com>
2026-09-26 00:01:50 +0900

    Fix production mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,11 +67,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: ENV.fetch("SMTP_ADDRESS"),
-    port: ENV.fetch("SMTP_PORT", 587),
-    domain: ENV.fetch("SMTP_DOMAIN"),
-    user_name: ENV.fetch("SMTP_USERNAME"),
-    password: ENV.fetch("SMTP_PASSWORD"),
+    address: ENV.fetch("SMTP_ADDRESS", "smtp.resend.com"),
+    port: ENV.fetch("SMTP_PORT", "587").to_i,
+    domain: ENV.fetch("SMTP_DOMAIN", "postapp.com"),
+    user_name: ENV.fetch("SMTP_USERNAME", "resend"),
+    password: ENV.fetch("SMTP_PASSWORD", ""),
     authentication: :plain,
     enable_starttls_auto: true
   }
